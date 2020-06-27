6ce90e71f3c2471a291bd787561c5de0c7b1c582
Kumar Shraiyash <justshraiyash@gmail.com>
2020-06-28 00:07:48 +0530

    country selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words:string = '';
+  limit:number = 10;
+
+  handleSlideChange( newLimit : number ){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0,this.limit).join(',');
+  }
 }
